3beffefa3ed5f10a9b805daa91f356fba1b01b4a
Thomas Stibor <thomas@stibor.net>
2019-02-20 21:10:18 +0100

    Use correct script filename in crontab

--- stage2-indigo/04-crontab-tweaks/01-run.sh
+++ stage2-indigo/04-crontab-tweaks/01-run.sh
@@ -3,5 +3,5 @@
 cat >> "${ROOTFS_DIR}/etc/cron.d/indigo" <<EOL
 SHELL=/bin/bash
 PATH=/usr/local/bin:/usr/local/sbin:/sbin:/usr/sbin:/bin:/usr/bin
-*/30 * * * * indigo_rpi.sh --reset-wifi-server > /dev/null 2>&1
+*/30 * * * * s_rpi_ctrl.sh --reset-wifi-server > /dev/null 2>&1
 EOL
