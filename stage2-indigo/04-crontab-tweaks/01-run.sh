6242028c64c51279c2cac823d357a3e47163ba8f
Thomas Stibor <thomas@stibor.net>
2019-02-21 21:40:10 +0100

    Enable LSB name support in /etc/cron.d/

--- stage2-indigo/04-crontab-tweaks/01-run.sh
+++ stage2-indigo/04-crontab-tweaks/01-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-cat >> "${ROOTFS_DIR}/etc/cron.d/indigo" <<EOL
+cat > "${ROOTFS_DIR}/etc/cron.d/indigo" <<EOL
 SHELL=/bin/bash
 PATH=/usr/local/bin:/usr/local/sbin:/sbin:/usr/sbin:/bin:/usr/bin
 */30 * * * * s_rpi_ctrl.sh --reset-wifi-server > /dev/null 2>&1
+EOL
+
+cat >> "${ROOTFS_DIR}/etc/default/cron" <<EOL
+EXTRA_OPTS="-l"
 EOL
