af1a915a4b1cd78d8aa15327f852e356e2804191
Thomas Stibor <t.stibor@gsi.de>
2019-03-05 23:15:48 +0100

    Switch crontab entries to /var/spool/cron/crontabs/indigo

--- stage2-indigo/04-crontab-tweaks/01-run.sh
+++ stage2-indigo/04-crontab-tweaks/01-run.sh
@@ -1,17 +1,7 @@
 #!/bin/bash -e
 
-cat > "${ROOTFS_DIR}/etc/cron.d/indigo_wifi_reset" <<EOL
-SHELL=/bin/bash
-PATH=/usr/local/bin:/usr/local/sbin:/sbin:/usr/sbin:/bin:/usr/bin
-*/30 * * * * indigo s_rpi_ctrl.sh --reset-wifi-server > /dev/null 2>&1
-EOL
+install -m 600 files/indigo "${ROOTFS_DIR}/var/spool/cron/crontabs"
 
-cat > "${ROOTFS_DIR}/etc/cron.d/indigo_update" <<EOL
-SHELL=/bin/bash
-PATH=/usr/local/bin:/usr/local/sbin:/sbin:/usr/sbin:/bin:/usr/bin
-@reboot indigo sudo apt-get update > /dev/null 2>&1
-EOL
-
-cat >> "${ROOTFS_DIR}/etc/default/cron" <<EOL
-EXTRA_OPTS="-l"
-EOL
+on_chroot << EOF
+chown indigo.crontab /var/spool/cron/crontabs/indigo
+EOF
