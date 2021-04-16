1c2d171eeb9597d1c8e6caa451c4bf10b24d6085
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-04-16 10:47:27 +0300

    show arm32 or 64 on welcome screen

--- indigosky/06-login-tweaks/00-run.sh
+++ indigosky/06-login-tweaks/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-install -m 644 files/neofetch.conf      "${ROOTFS_DIR}/etc/"
-install -m 644 files/indigosky_logo.txt "${ROOTFS_DIR}/etc/"
+install -m 644 files/neofetch.conf        "${ROOTFS_DIR}/etc/"
+install -m 644 files/indigosky_logo32.txt "${ROOTFS_DIR}/etc/"
+install -m 644 files/indigosky_logo64.txt "${ROOTFS_DIR}/etc/"
 
 echo "neofetch --config /etc/neofetch.conf" >> "${ROOTFS_DIR}/home/indigo/.bashrc"
