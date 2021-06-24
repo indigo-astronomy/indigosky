1c3709d99439931cb5625d7c1fd10eba0f3d2c57
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-06-25 02:19:51 +0300

    enable PWM for the GPIO driver

--- indigosky/04-sys-tweaks/00-run.sh
+++ indigosky/04-sys-tweaks/00-run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
+# Enable PWM for the RPi GPIO driver
+echo 'dtoverlay=pwm-2chan' >>"${ROOTFS_DIR}/boot/config.txt"
+
 install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
 install -m 644 files/dphys-swapfile	"${ROOTFS_DIR}/etc/dphys-swapfile"
