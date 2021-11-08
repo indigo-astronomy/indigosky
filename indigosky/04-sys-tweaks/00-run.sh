a9568bf563049595ed0cce6c30bda0158c9c203c
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-11-08 18:32:56 +0200

    add GPIO_07 as a shutdown signal and GPIO_08 as a shutdown complete feedback

--- indigosky/04-sys-tweaks/00-run.sh
+++ indigosky/04-sys-tweaks/00-run.sh
@@ -3,5 +3,9 @@
 # Enable PWM for the RPi GPIO driver
 echo 'dtoverlay=pwm-2chan' >>"${ROOTFS_DIR}/boot/config.txt"
 
+# Enable poweroff from GPIO pins
+echo 'dtoverlay=gpio-shutdown,gpio_pin=7,active_low=1,gpio_pull=up' >>"${ROOTFS_DIR}/boot/config.txt"
+echo 'dtoverlay=gpio-poweroff,gpiopin=8,active_low=1' >>"${ROOTFS_DIR}/boot/config.txt"
+
 install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
 install -m 644 files/dphys-swapfile	"${ROOTFS_DIR}/etc/dphys-swapfile"
