11b1a75a6ff9c2b3fa906f8cb2ab687356b1d216
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-11-10 14:26:01 +0200

    allow new rpis to run as fast as possible

--- indigosky/04-sys-tweaks/00-run.sh
+++ indigosky/04-sys-tweaks/00-run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -e
 
-# Enable PWM for the RPi GPIO driver
+echo '# Enable PWM for the RPi GPIO driver' >>"${ROOTFS_DIR}/boot/config.txt"
 echo 'dtoverlay=pwm-2chan' >>"${ROOTFS_DIR}/boot/config.txt"
 
-# Enable poweroff from GPIO pins
+echo '# Enable poweroff from GPIO pins' >>"${ROOTFS_DIR}/boot/config.txt"
 echo 'dtoverlay=gpio-shutdown,gpio_pin=7,active_low=1,gpio_pull=up' >>"${ROOTFS_DIR}/boot/config.txt"
 echo 'dtoverlay=gpio-poweroff,gpiopin=8,active_low=1' >>"${ROOTFS_DIR}/boot/config.txt"
+
+echo '[pi4]' >>"${ROOTFS_DIR}/boot/config.txt"
+echo '# Run as fast as firmware / board allows' >>"${ROOTFS_DIR}/boot/config.txt"
+echo 'arm_boost=1' >>"${ROOTFS_DIR}/boot/config.txt"
+
 
 install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
 install -m 644 files/dphys-swapfile	"${ROOTFS_DIR}/etc/dphys-swapfile"
